1d056e397d617b54ae67f4b7df41ef773f757c94
Senam Daniel <122734888+senamcancode@users.noreply.github.com>
2023-10-17 13:38:39 +0100

    Update CalculatorTests.java

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName(value = "The Calculator should")
 public class CalculatorTests {
@@ -22,5 +23,46 @@ public class CalculatorTests {
         final Integer difference = Calculator.subtract(2, 4);
 
         assertEquals(-2, difference);
+    }
+
+    @Test 
+    @DisplayName("multiply two numbers")
+    public void testMultiplication(){
+        final Integer product = Calculator.multiply(2, 4);
+
+        assertEquals(8, product);
+    }
+
+    @Test 
+    @DisplayName("multiply negative numbers")
+    public void testNegativeMultiplication(){
+        final Integer product = Calculator.multiply(-1, -1);
+
+        assertEquals(1, product); 
+    }
+
+    @Test 
+    @DisplayName("multiply one negative")
+    public void testNegativeAndPositiveMultiplication(){
+        final Integer product = Calculator.multiply(-1, 2);
+
+        assertEquals(-2, product); 
+    }
+
+    @Test
+    @DisplayName("divide two numbers")
+    public void testDivision(){
+        final Integer division = Calculator.divide(10, 2);
+
+        assertEquals(10, division);
+    }
+
+    @Test
+    @DisplayName("divide by 0 throws exception")
+    public void testDivideByZero() {
+        
+        assertThrows(ArithmeticException.class, () => {
+            Calculator.divide(10, 0);
+        }) 
     }
 }
